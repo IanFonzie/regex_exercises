a12211d2832c868ffb9be30a8367a453c26e95dd
IanFonzie <fberg_i@hotmail.com>
2017-09-15 17:13:08 -0700

    Modified using-regular-expressions-in-ruby-and-javascript.js

--- using-regular-expressions-in-ruby-and-javascript.js
+++ using-regular-expressions-in-ruby-and-javascript.js
@@ -45,4 +45,19 @@ function formatDate2(dateString) {
 	return dateString.replace(/^(\d\d\d\d)([\-\/])(\d\d)\2(\d\d)$/, '$4.$3.$1');
 }
 
+// I got their second solution. They opted to split it up into two smaller problems:
+// one that handles dates in 2016-05-17 format and one that handles 2016/05/17 format,
+// which is what both our primary solutions do. The only possible gotcha here si that 
+// you need to remember to escape the / characters in regex.
 
+// can also be solved with just one regex, as in the alternate solution, but at the 
+// expense of a more complex regex and lower readability. The regex adds one additional
+// capturing group to capture the first - or / and uses a \2 backreference to refer
+// back to the capture in the regex. Hwoever this additional capturing group modifies the
+// backreference numbers for the month and day components of the date so we now need to
+// refer to them as $4 and $3. IN ruby this might be a good time to look up how to use 
+// named capturing groups.
+
+// Alternate solution uses variables to store regex. done for readability and to show 
+// that they are no different from any other object; they can be manipulated and passed
+// around as needed.
